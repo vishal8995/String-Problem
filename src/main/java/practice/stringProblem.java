050e69e47764885113df0a520b128035873196f8
admin <admin@192.168.110.4>
2018-12-05 17:35:23 +0530

    Completed

--- src/main/java/practice/stringProblem.java
+++ src/main/java/practice/stringProblem.java
@@ -13,11 +13,14 @@ public class stringProblem {
 		Scanner sc = new Scanner(System.in);
 		int i;
 		Map<Character, Integer> valueMap = new LinkedHashMap<Character, Integer>();
+		Map<Character, Integer> evenMap = new LinkedHashMap<Character, Integer>();
+		Map<Character, Integer> oddMap = new LinkedHashMap<Character, Integer>();
 
 		System.out.println("Enter String !!");
 		String str = sc.nextLine();
 		char[] charArray = str.toCharArray();
 
+		// saving values in HashMap
 		for (i = 0; i < charArray.length; i++) {
 
 			if (valueMap.isEmpty()) {
@@ -43,6 +46,30 @@ public class stringProblem {
 
 		System.out.println(valueMap);
 
+		// removing even values and printing rest in map as well as string format
+		for (Map.Entry<Character, Integer> entry : valueMap.entrySet()) {
+
+			if (entry.getValue() % 2 == 0) {
+
+				evenMap.put(entry.getKey(), entry.getValue());
+			}
+
+			else {
+
+				oddMap.put(entry.getKey(), entry.getValue());
+			}
+
+		}
+
+		System.out.println(evenMap);
+		
+		for (Map.Entry<Character, Integer> entry : oddMap.entrySet()) {
+			
+			for (i=0; i<entry.getValue(); i++) {
+				
+				System.out.print(entry.getKey());
+			}
+		}
 	}
 
 }
